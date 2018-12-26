1428d9647ffbb77a8160877434abd2075117f1ff
DEJA-TH <denduang.cs@gmail.com>
2018-12-26 10:25:15 +0700

    yyy

---
+++ src/components/login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="gg">
+        <h2>{{bb}}</h2>
+        </div>    
+</template>
+
+<script>
+export default {
+    name:"Yhaaaa"
+}
+</script>
+
+<style scoped>
+
+</style>
+
